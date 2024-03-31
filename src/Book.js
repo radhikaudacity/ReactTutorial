df68b32ae9c9a6689e7527e1a8c29556aa276c8e
Radhika <radhika_rb@hotmail.com>
2024-03-31 16:41:26 +0530

    Sendinf fx as parameter in component.

--- src/Book.js
+++ src/Book.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-const Book = ({ author, title, id, img }) => (
-  <li>
-    <div style={{ textAlign: 'center' }}>
-      <h2>{author}</h2>
-      <h2>{title}</h2>
-      <h2>{id}</h2>
-    </div>
-    <img src={img}></img>
-  </li>
-);
+const Book = ({ author, title, id, img, displayTitle }) => {
+  const displayValue = () => displayTitle(id);
+  return (
+    <li>
+      <div style={{ textAlign: 'center' }}>
+        <h2>{author}</h2>
+        <h2>{title}</h2>
+        <h2>{id}</h2>
+        <button onClick={displayValue}>displayTitle</button>
+      </div>
+      <img src={img}></img>
+    </li>
+  );
+};
 
 export default Book;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import Book from './Book';
 import books from './books.js';
 
+const displayTitle = (id) =>
+  console.log(books.find((book) => book.id === id).title);
+
 const BookList = () => (
-  <ul>
-    {books.map((book) => (
-      <Book {...book} key={book.id} />
-    ))}
-  </ul>
+  <>
+    <ul>
+      {books.map((book) => (
+        <Book {...book} key={book.id} displayTitle={displayTitle} />
+      ))}
+    </ul>
+  </>
 );
 
 export default BookList;
