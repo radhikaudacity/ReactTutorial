2dfabc4f5cb17dd6c30f4027c125e451946c32cd
Radhika <radhika_rb@hotmail.com>
2024-03-31 16:46:46 +0530

    Restructuring.

--- src/Book.js
+++ src/Book.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
-const Book = ({ author, title, id, img, displayTitle }) => {
-  const displayValue = () => displayTitle(id);
-  return (
-    <li>
-      <div style={{ textAlign: 'center' }}>
-        <h2>{author}</h2>
-        <h2>{title}</h2>
-        <h2>{id}</h2>
-        <button onClick={displayValue}>displayTitle</button>
-      </div>
-      <img src={img}></img>
-    </li>
-  );
-};
+const Book = ({ author, title, id, img, displayTitle }) => (
+  <li>
+    <div style={{ textAlign: 'center' }}>
+      <h2>{author}</h2>
+      <h2>{title}</h2>
+      <h2>{id}</h2>
+      <button onClick={() => displayTitle(id)}>displayTitle</button>
+    </div>
+    <img src={img}></img>
+  </li>
+);
 
 export default Book;
